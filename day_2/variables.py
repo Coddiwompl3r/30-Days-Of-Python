07199de74fcac72bad36c5a705e26bda223fcec1
Sebastian Hines <sebastianhines@icloud.com>
2026-06-07 17:31:53 -0400

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,14 @@
+# Day 2: 30 Days of python programming
+
+# Level 1
+
+first_name = 'Sebastian'
+last_name = 'Hines'
+full_name = 'Sebastian Hines'
+country = 'United States'
+state = 'Florida'
+city = 'Fort Myers'
+age = 17
+year = '2026'
+is_married = False
 
